3aa7d6da0376e0beeba68495d49de78639b6785e
Jeremy <l.saihong25@gmail.com>
2023-04-22 19:20:28 +0800

    Revert "style: massive change the looks of the website"
    This reverts commit b0003d9a02a24e2ed727a9e448b4d37c0ca0cb61.

--- dga/authentication.py
+++ dga/authentication.py
@@ -1,69 +1,49 @@
-from flask import (
-    Flask,
-    render_template,
-    url_for,
-    request,
-    redirect,
-    flash,
-    session,
-    Blueprint,
-)
+from flask import Flask, render_template, url_for, request, redirect, flash, session, Blueprint
 from dga import auth
 from .rtdatabase import db
 
-authentication = Blueprint("authentication", __name__)
+authentication = Blueprint('authentication', __name__)
 
-
-@authentication.route("/", methods=["GET", "POST"])
+@authentication.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        session.pop("_flashes", None)  # clear flash messages
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        session.pop('_flashes', None) # clear flash messages 
 
         try:
             user = auth.sign_in_with_email_and_password(email, password)
             # print(auth.get_account_info(user['idToken']))
             # print(auth.current_user)
-            session["idToken"] = user["idToken"]
-
-            user = auth.get_account_info(session["idToken"])["users"][0]
-
-            if db.child("records").child(user["localId"]).get().val() is None:
-                # User record doesn't exist, creating a template
-                dga_typelist = ["dt1", "dt4", "dt5", "dp1", "dp2"]
-                new_data = dict(zip(dga_typelist, [0] * len(dga_typelist)))
-
-                db.child("records").child(user["localId"]).set({"2023": new_data})
-
-            return redirect(url_for("graph.overview", type="dt1"))
-        except:
-            flash("Invalid username/password combination", "error")
-            return redirect(url_for("authentication.index"))
+            session['idToken'] = user['idToken']
+            return redirect(url_for('rtdatabase.home'))
+        except: 
+            flash('Invalid username/password combination', 'error')
+            return redirect(url_for('authentication.index'))
     else:
-        return render_template("index.html")
+        return render_template('index.html')
 
 
-@authentication.route("/logout")
+@authentication.route('/logout')
 def logout():
     auth.current_user = None
-    session.pop("idToken", None)
-    print("User logged out")
-    return redirect(url_for("authentication.index"))
+    session.pop('idToken', None)
+    print('User logged out')
+    return redirect(url_for('authentication.index'))
 
 
-@authentication.route("/registration", methods=["GET", "POST"])
+@authentication.route('/registration', methods=['GET', 'POST'])
 def registration():
-    if request.method == "POST":
+    if request.method == 'POST':
         # we can use request.form.get() to avoid getting errors if the form field is empty
         # with this we will get None instead
         # email = request.form.get('email')
         # password = request.form.get('password')
         # confirm_password = request.form.get('cpassword')
-        email = request.form["email"]
-        password = request.form["password"]
-        confirm_password = request.form["cpassword"]
-        session.pop("_flashes", None)  # clear flash messages
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['cpassword']
+        session.pop('_flashes', None) # clear flash messages 
 
         try:
             # Password must be looooooooong
@@ -73,22 +53,21 @@ def registration():
             #     return redirect(url_for('registration'))
 
             if password != confirm_password:
-                flash("Passwords do not match", "error")
-                return redirect(url_for("authentication.registration"))
+                flash('Passwords do not match', 'error')
+                return redirect(url_for('authentication.registration'))
 
             auth.create_user_with_email_and_password(email, password)
-
-            flash("Registration successful. You can login now.", "msg")
-            return redirect(url_for("authentication.index"))
-        except Exception as e:
+            
+            flash('Registration successful. You can login now.', 'msg')
+            return redirect(url_for('authentication.index'))
+        except Exception as e: 
             print(e)
-            flash("Email already exists", "error")
-            return redirect(url_for("authentication.registration"))
+            flash('Email already exists', 'error')
+            return redirect(url_for('authentication.registration'))
     else:
-        return render_template("registration.html")
-
+        return render_template('registration.html')
 
 # def get_user_id_by_email(email):
 #     user = auth.get_account_info(auth.current_user['idToken'])['localId']
-
+    
 #     return user
